a3a22001eea4ef4ad77b06894d7690428fc83a6a
Azimov Parviz <parviz.azimov@student.lamk.fi>
2020-09-21 14:28:14 +0300

    .gitignore and sidebar added
    Signed-off-by: Azimov Parviz <parviz.azimov@student.lamk.fi>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/sidebar.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to WIP resume of Parviz Azimov!!!</h1>
+	<h1>Welcome to WIP resume of Parviz Azimov!!!</h1>
+	<Sidebar/>
     </div>
   );
 }
---
+++ src/components/sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+
+function Sidebar() {
+  return (
+    <div className="App">
+	<div class="sidenav">
+	    <a href="#about">About</a>
+	    <a href="#contact">Contact</a>
+	</div>
+    </div>
+  );
+}
+
+export default Sidebar;
