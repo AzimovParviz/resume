568277330deb5a3fe97ef01c9ae0f271b28f7fbf
Azimov Parviz <parviz.azimov@student.lamk.fi>
2020-12-08 16:06:00 +0200

    Updates

---
+++ precache-manifest.8e1274a9ff277e2174320ac08d6483bc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd361a75144c2982d38728c9f9468300",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "38ff7d84c2afeaad7907",
+    "url": "/resume/static/css/main.9e896da3.chunk.css"
+  },
+  {
+    "revision": "acf5fe7e0e9631e82a89",
+    "url": "/resume/static/js/2.7aab3103.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resume/static/js/2.7aab3103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38ff7d84c2afeaad7907",
+    "url": "/resume/static/js/main.5e2d065a.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  },
+  {
+    "revision": "f9aef0d0592d4bb9a9e9d992584b1cfa",
+    "url": "/resume/static/media/bg.f9aef0d0.png"
+  },
+  {
+    "revision": "d1b57ccb64569bd40b14ae43e9bf8e8a",
+    "url": "/resume/static/media/portrait.d1b57ccb.jpg"
+  },
+  {
+    "revision": "be3684f78926978504ea804ec09481a2",
+    "url": "/resume/static/media/site-bg.be3684f7.jpg"
+  }
+]);
