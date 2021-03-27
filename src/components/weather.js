260897b298fe1cdef0d83fccef07cbccb411382a
Azimov Parviz <parviz.azimov@student.lamk.fi>
2021-03-27 10:27:24 +0200

    can fetch the weather without doing repeated requests to the API

--- src/components/weather.js
+++ src/components/weather.js
@@ -4,16 +4,7 @@ import axios from 'axios';
 function Weather() {
     const call = "https://api.openweathermap.org/data/2.5/weather?q=Helsinki&appid=0e86c819815dc35bc90fc81863a37471";
     const [weather, setWeather] = useState({});
-    //const promise = axios.get(call);
-    //console.log(promise);
-    const weather_data = "";
-    
-    // promise.then(response => {
-    //     const weather_data = response.data.main;
-    //     setWeather = response.data.main;
-    //     console.log(response.data.main);
-    //     console.log("weather is:  ",weather_data);
-    // });
+    const weather_data = "";       
     const hook = () => {
         console.log('effect');
         axios
@@ -24,11 +15,11 @@ function Weather() {
                 console.log(response.data.main);                
             });
     };
-    useEffect(hook);
+    useEffect(hook, {});
     
     return (
         <div className="weather">
-          <p>Temperature in Helsinki is: {weather.temp}</p>
+          <p>Temperature in Helsinki is: {(weather.temp-273.15).toFixed(1)} Celcius</p>
         </div>
     );
 }
